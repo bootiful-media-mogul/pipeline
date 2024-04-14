8c035c821de939687813b373422b21f12ce7ffe5
Josh Long <josh@joshlong.com>
2024-04-14 12:40:50 -0700

    polish

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,36 @@
 #!/usr/bin/env bash
 
-kubectl get pods
+# this pipeline deploys everything, altogether. if the assets are already
+# deployed then its idempotent and nothing should happen. right?
 
-kubectl apply -f $GITHUB_WORKSPACE/bin/k8s
+export ROOT_DIR="$(cd `dirname $0` && pwd )"
+echo "The root directory is ${ROOT_DIR}."
+export NAMESPACE_NAME=mogul
+
+AUTHORIZATION_SERVICE_IP=${NAMESPACE_NAME}-authorization-service-ip
+
+create_ip(){
+  ipn=$1
+  if [ -z "$ipn" ]; then
+    echo "you didn't specify the name of the IP address to create "
+  else
+    gcloud compute addresses list --format json | jq '.[].name' -r | grep $ipn || gcloud compute addresses create $ipn --global
+  fi
+}
+
+write_secrets(){
+  export SECRETS=${NAMESPACE_NAME}-secrets
+  SECRETS_FN=$HOME/${SECRETS}
+  mkdir -p "`dirname $SECRETS_FN`"
+  cat <<EOF >${SECRETS_FN}
+MESSAGE=hik8s
+EOF
+  kubectl delete secrets $SECRETS || echo "no secrets to delete."
+  kubectl create secret generic $SECRETS --from-env-file $SECRETS_FN
+}
+
+write_secrets
+
+create_ip ${AUTHORIZATION_SERVICE_IP}
+
+kubectl apply -f $ROOT_DIR/k8s
