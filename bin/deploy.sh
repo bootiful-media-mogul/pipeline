469e60a79bf52f8524b723838cbe4eb2247722cc
Josh Long <josh@joshlong.com>
2024-04-22 16:39:33 -0700

    jq

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -70,9 +70,10 @@ done
 cd $ROOT_DIR/k8s/carvel/
 
 get_image(){
+  jq --version
   deployment_name=$1
   echo "going to get the image for $deployment_name"
-  kubectl get "$deployment_name" -o json  | jq -r '.spec.template.spec.containers'
+  kubectl get "$deployment_name" -o json  | jq -r '.spec.template.spec.containers.[0].image'
 }
 
 for f in authorization-service mogul-service mogul-gateway  ; do
